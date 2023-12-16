45685d66a35a5b4f64f4ddf52329f94e878578e7
tohid <tohidpatel584@gmail.com>
2023-12-16 18:30:30 +0530

    Added correct way of debug=false in production

--- website/settings.py
+++ website/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG')
+DEBUG = os.getenv('DEBUG') == 'True'
 # Application definition
 
 INSTALLED_APPS = [
